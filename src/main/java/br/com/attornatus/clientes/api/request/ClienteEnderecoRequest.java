798611fa6a3d8317b2ecf0b5b2945617220c2ed0
administrador <gilberto.tec@vivaldi.net>
2023-01-04 22:29:02 -0300

    init: validacao das request com i18n para as menssagens de erro

--- src/main/java/br/com/attornatus/clientes/api/request/ClienteEnderecoRequest.java
+++ src/main/java/br/com/attornatus/clientes/api/request/ClienteEnderecoRequest.java
@@ -1,14 +1,20 @@
 package br.com.attornatus.clientes.api.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class ClienteEnderecoRequest {
-
+	
+	@NotBlank(message = "CEP é Obrigatorio")
 	private String cep;
+	@NotBlank(message = "Cidade é Obrigatorio")
 	private String cidade;
+	@NotBlank(message = "numero é Obrigatorio")
 	private String numero;
+	@NotBlank(message = "Logradouro é Obrigatorio")
 	private String logradouro;
+	
 	private Boolean principal;
 
 }
--- src/main/java/br/com/attornatus/clientes/api/request/ClienteRequest.java
+++ src/main/java/br/com/attornatus/clientes/api/request/ClienteRequest.java
@@ -4,14 +4,18 @@ import java.util.Date;
 import java.util.List;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class ClienteRequest{
 
-	@NotBlank(message = "Nome is mandatory")
+	@NotBlank(message = "Nome é Obrigatorio")
 	private String nome;
+
+	@NotNull(message = "dtNasciemnto é Obrigatorio")
 	private Date dtNascimento;
+	
 	private List<ClienteEnderecoRequest> listEnderecos;
 	
 }
